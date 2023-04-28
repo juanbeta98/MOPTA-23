ea0a1fd8534705b05e14f2e8c11e6d47f820a12f
juanbeta98 <juan.beta98@gmail.com>
2023-04-28 10:13:21 -0500

    Create first_second_stages file Upload first stage model

---
+++ first_second_stages.py
@@ -0,0 +1,22 @@
+from gurbi import *
+
+
+def first_stage(S, c_f):
+
+    model = Model('first stage')
+    N = range(1,9)
+
+    x = dict()
+    x.update({(n,s): model.addVar(vtype=GRB.BINARY, name=f'x_{n}{s}')} for n in N for s in S)
+
+    model.addConstr(quicksum(x[n,s] for n in N for s in S) == 600)
+
+    for s in S:
+        model.addConstr(quicksum(x[n,s] for n in N) <= 1)
+    
+    model.setObjective(quicksum(c_f*n*x[n,s] for n in N for s in S))
+
+    model.update()
+    model.setParam('OutputFlag',0)
+    model.optimize()
+    
