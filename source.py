ce8c58be88e8dc71bea0ce7de6535fd96f0d5d56
ari-rojas <ari_rojasa@hotmail.com>
2023-07-21 16:39:32 -0500

    Sensitivity 1

--- source.py
+++ source.py
@@ -301,6 +301,77 @@ def second_stage_ESPP(S,K,K_s,S_k,T,y,a,t):
 
     return mpsol, mp.getObjective().getValue()
 
+def test_scenario_sensitivity(S,K,K_s,n,a,t):
+
+    mp = gb.Model("Restricted Master Problem")
+
+    dummy_0 = {k:mp.addVar(vtype=gb.GRB.CONTINUOUS, obj=1, name=f"st0_{k}") for k in K}
+    aux = {s:mp.addVar(vtype=gb.GRB.CONTINUOUS, obj=2, name=f"aux_{s}") for s in S}
+
+    vehic_assign = {}
+    for k in K:
+        vehic_assign[k] = mp.addConstr(dummy_0[k] >= 1, f"V{k}_assignment")
+
+    st_conv = {}
+    for s in S:
+        st_conv[s] = mp.addConstr(aux[s] <= n[s], f"S{s}_convexity")
+
+    mp.setParam("OutputFlag",0)
+
+    time0 = process_time(); i = 0
+    lbd = {s:list() for s in S}
+    columns = {s:list() for s in S}
+
+    while True:
+        
+        mp.update()
+        mp.optimize()
+
+        pi = {k:vehic_assign[k].getAttr("Pi") for k in K}
+        sigmas = {s:st_conv[s].getAttr("Pi") for s in S}
+
+        print(f"\t\tIteration {i}:\t\tMP obj: {round(mp.getObjective().getValue(),2)}\ttime: {round(process_time()-time0,2)}s")
+        i += 1
+
+        opt = 0
+        for s in S:
+            
+            V,A,rc = get_graph(s,K_s[s],a,pi,sigmas[s])
+            ext = vertices_extensions(V,A)
+
+            routes_DFS = []; covered_nodes = set()
+            label_DFS(v="s",rP=0,tP=0,qP=0,P=[],cK=covered_nodes,L=routes_DFS,s=s,r=rc,t=t,a=a,ext=ext)
+            opt += len(routes_DFS)
+
+            for l in routes_DFS:
+                col = {k:1 if k in l else 0 for k in K}
+                new_Col = gb.Column(col.values(),vehic_assign.values())
+                lbd[s].append(mp.addVar(vtype=gb.GRB.CONTINUOUS,name=f"lbd_{s}_{len(lbd[s])}",obj=0,ub=1,column=new_Col))
+                columns[s].append(l)
+
+                mp.chgCoeff(st_conv[s],lbd[s][-1],1)
+
+        if opt == 0: mpsol = mp.getObjective().getValue(); break
+    
+    for v in mp.getVars():
+        v.vtype = gb.GRB.BINARY
+    if mpsol > 0: mp.setParam("MIPGap",0.01)
+    mp.setParam("MIPFocus",2)
+    mp.update(); mp.optimize()
+
+    results = {"routes":{s:[] for s in S}, "total_serviced_customers":{s:[] for s in S}, "total_total":list(), "infeasible":list()}
+    for s in S:
+        for l in range(len(lbd[s])):
+            if lbd[s][l].X > 0.5:
+                results["routes"][s] += [columns[s][l]]
+                results["total_serviced_customers"][s] += columns[s][l]
+                results["total_total"] += columns[s][l]
+    for k in K:
+        if dummy_0[k].X > 0.5:
+            results["infeasible"].append(k)
+
+    return results
+
 
 def second_stage_chargers(S,K,K_s,y,a,t,sc):
 
